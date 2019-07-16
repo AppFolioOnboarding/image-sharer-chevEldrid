68991e146aabba64bde5b1d29cbe7dc89f10a7a4
Chev Eldrid <sacheverel.eldrid@appfolio.com>
2019-07-16 09:59:12 -0700

    add default db values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# rubocop:disable LineLength
+Image.create([
+  { title: 'Pineapple', imageLink: 'https://images-na.ssl-images-amazon.com/images/I/81WJyO53YAL._SY550_.jpg' },
+  { title: 'Return of the Pineapple', imageLink: 'https://upload.wikimedia.org/wikipedia/commons/7/74/%E0%B4%95%E0%B5%88%E0%B4%A4%E0%B4%9A%E0%B5%8D%E0%B4%9A%E0%B4%95%E0%B5%8D%E0%B4%95.jpg' },
+  { title: 'Psych', imageLink: 'https://static01.nyt.com/images/2014/03/26/watching/psych-watching-recommendation/psych-watching-recommendation-videoSixteenByNineJumbo1600-v4.jpg' }
+])
+# rubocop:enable LineLength
